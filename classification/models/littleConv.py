f96b5c726651e44a05a6e5a9edb37c09e7aece13
Yangcy <13520183356@139.com>
2019-06-07 10:29:47 +0800

    added LittlePoolingConv

--- classification/models/littleConv.py
+++ classification/models/littleConv.py
@@ -65,69 +65,65 @@ class littleConv(torch.nn.Module):
         return self.linear(h)
 
 
-# class littleConv(nn.Module):
-#     """
-#     Simple conv. VAE
+class littlePoolingConv(torch.nn.Module):
+    """
+    Class encoder
 
-#     """
-#     def __init__(self, c_dim, z_dim, num_channels, std=0.02):
-#         """
-#         Initialization
+    """
+    def __init__(self, c_dim, z_dim=200, num_channels=1, std=0.02):
+        """
+        Initialization
+        conv -> ReLU x 4 -> (mu, sigma)
 
-#         """
-#         super(ConvVAE, self).__init__()
-#         self.c_dim = c_dim
-#         self.z_dim = z_dim
-#         self.num_channels = num_channels
-#         self.std = std
-#         self.gpu_avaliable = torch.cuda.is_available()
+        """
+        super(littlePoolingConv, self).__init__()
+        self.main = nn.Sequential(
+			nn.Conv2d(num_channels, 64, 3, 1, 1, bias=True), # b x 64 x 14 x 14
+			nn.ReLU(True),
+            nn.MaxPool2d(2),
+			nn.BatchNorm2d(64),
+			nn.Conv2d(64, 128, 3, 1, 1, bias=False), # b x 128 x 7 x 7
+			nn.ReLU(True),
+            nn.MaxPool2d(2),
+			nn.BatchNorm2d(128),
+			nn.Conv2d(128, 256, 3, 1, 1, bias=False), # b x 256 x 3 x 3
+			nn.ReLU(True),
+            nn.MaxPool2d(2),
+			nn.BatchNorm2d(256),
+			nn.Conv2d(256, 128, 3, 1, 1, bias=False), # b x 128 x 1 x 1
+			nn.ReLU(True),
+            nn.MaxPool2d(2),
+			nn.BatchNorm2d(128)
+			)
 
-#         self.model_name = "ConvVAE"
-#         self.Encoder = Encoder(c_dim, z_dim, num_channels, std)
-#         self.Decoder = Decoder(c_dim, z_dim, num_channels, std)
+        self.c_dim = c_dim
+        self.z_dim = z_dim
+        self.num_channels = num_channels
+        self.std = std
+        self.linear= nn.Linear(int(np.prod(c_dim)), z_dim)
+        self.init_weights()
 
-#     def parameters(self):
-#         """
-#         Overwrite parameters
+    def init_weights(self):
+        """
+        Weight Initialization
 
-#         """
-#         return chain(self.Encoder.parameters(), self.Decoder.parameters())
+        """
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.normal_(0.0, self.std)
+                m.bias.data.normal_(0.0, self.std)
+            elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                m.weight.data.normal_(0.0, self.std)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.normal_(1.0, self.std)
+                m.bias.data.zero_()
 
-#     def sample_from_q(self, mu, log_sigma):
-#         """
-#         Simulation of sample process
+    def forward(self, input):
+        """
+        Definition of forward process
 
-#         """
-#         if self.gpu_avaliable and isinstance(mu, torch.cuda.FloatTensor):
-#             epsilon = Variable(torch.randn(mu.size()), requires_grad=False).type(torch.FloatTensor).cuda()
-#         else:
-#             epsilon = Variable(torch.randn(mu.size()), requires_grad=False).type(torch.FloatTensor)
-#         sigma = torch.exp(log_sigma / 2)
-#         return mu + sigma * epsilon
+        """
+        h = self.main(input)
+        h = h.resize(h.size(0), h.size(1) * h.size(2) * h.size(3))
+        return self.linear(h)
 
-#     def forward(self, x):
-#         """
-#         Definition of forward process
-
-#         """
-#         self.mu, self.log_sigma = self.Encoder(x)
-#         z = self.sample_from_q(self.mu, self.log_sigma)
-#         return self.Decoder(z)
-
-#     def load(self, path):
-#         """
-#         load model from given path
-
-#         """
-#         self.load_state_dict(torch.load(path))
-
-#     def save(self, dataset, name=None):
-#         """
-#         save model to given path with time as name
-
-#         """
-#         if name is None:
-#             prefix = 'checkpoints/' + self.model_name + '_' + str(self.z_dim) + '_' + dataset + '_'
-#             name = time.strftime(prefix + '%m%d_%H:%M:%S.pth')
-#         torch.save(self.state_dict(), name)
-#         return name
